5638928728e9eee85948fd814f65428cee6a1c45
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-01-06 13:51:32 +0900

    콜백 함수 없는 비동기 코드 작성법

---
+++ Typescript Starter Kit_Hacker News Migration/10. 콜백 함수 없는 비동기 코드 작성법/src/core/api.ts
@@ -0,0 +1,36 @@
+import { NewsFeed, NewsDetail } from "../types";
+
+export default class Api {
+  xhr: XMLHttpRequest;
+  url: string;
+
+  constructor(url: string) {
+    this.xhr = new XMLHttpRequest();
+    this.url = url;
+  }
+
+  async request<AjaxResponse>(): Promise<AjaxResponse> {
+    const response = await fetch(this.url);
+    return (await response.json()) as AjaxResponse;
+  }
+}
+
+export class NewsFeedApi extends Api {
+  constructor(url: string) {
+    super(url);
+  }
+
+  async getData(): Promise<NewsFeed[]> {
+    return this.request<NewsFeed[]>();
+  }
+}
+
+export class NewsDetailApi extends Api {
+  constructor(url: string) {
+    super(url);
+  }
+
+  async getData(): Promise<NewsDetail> {
+    return this.request<NewsDetail>();
+  }
+}
